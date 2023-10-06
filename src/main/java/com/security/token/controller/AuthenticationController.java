be9de12f9eac2cfbfbec7d3ba0ef639a955eb8f4
Ben Hsieh <bqh5026@gmail.com>
2023-10-05 23:40:50 -0500

    Edit authentication controller

--- src/main/java/com/security/token/controller/AuthenticationController.java
+++ src/main/java/com/security/token/controller/AuthenticationController.java
@@ -1,5 +1,12 @@
 package com.security.token.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthenticationController {
+	
+	public String getAuthentication() {
+		return "Authentication sucessful!"; 
+	}
 
 }
