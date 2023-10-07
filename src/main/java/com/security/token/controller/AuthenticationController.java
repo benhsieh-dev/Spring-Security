f7252667475fd5e34958858561441cf91abf412d
Ben Hsieh <bqh5026@gmail.com>
2023-10-06 22:07:18 -0500

    Update authentication controller

--- src/main/java/com/security/token/controller/AuthenticationController.java
+++ src/main/java/com/security/token/controller/AuthenticationController.java
@@ -1,10 +1,12 @@
 package com.security.token.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
 	
+	@GetMapping("/security")
 	public String getAuthentication() {
 		return "Authentication sucessful!"; 
 	}
