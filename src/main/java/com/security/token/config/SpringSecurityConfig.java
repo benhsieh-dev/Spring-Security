74358e637d26511ea8d9eb8812ea70111698e59c
Ben Hsieh <bqh5026@gmail.com>
2023-10-11 22:46:41 -0500

    Update spring security configuration

--- src/main/java/com/security/token/config/SpringSecurityConfig.java
+++ src/main/java/com/security/token/config/SpringSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.security.token.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfig {
-	public InMemoryUserDetailsManager userDetailService() {
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("username")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user); 
 		
 	}
 
