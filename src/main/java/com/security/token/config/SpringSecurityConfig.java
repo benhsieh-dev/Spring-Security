e38fa0bf80b3456ea1588d822d8d36960d172c9a
Ben Hsieh <bqh5026@gmail.com>
2023-10-15 22:31:06 -0500

    Update spring security configuration

--- src/main/java/com/security/token/config/SpringSecurityConfig.java
+++ src/main/java/com/security/token/config/SpringSecurityConfig.java
@@ -2,9 +2,11 @@ package com.security.token.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -20,8 +22,13 @@ public class SpringSecurityConfig {
 		
 	}
 	
-	public SecurityFilterChain filterChan() {
-		
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz
+					.antMatchers("/message")
+					.authenticated()
+				)
+					.httpBasic();
+				return http.build(); 
 	}
 
 }
