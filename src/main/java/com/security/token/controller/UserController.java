57bfe4cd6090f9114357fc89346149f3a8b059d2
Ben Hsieh <bqh5026@gmail.com>
2023-10-19 20:43:25 -0500

    Update user controller

--- src/main/java/com/security/token/controller/UserController.java
+++ src/main/java/com/security/token/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.security.token.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+	@GetMapping("/user")
+	public String getMessage() {
+		return "Hello authenticated user"; 
+	}
 
 }
